23ac5fc01956ce9d5e52de379d414e11db99fcdc
Michael Chong <mikec0920@gmail.com>
2021-08-25 10:35:52 -0700

    Split javascript into separate file

---
+++ script.js
@@ -0,0 +1,60 @@
+function computerPlay() {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    default:
+      return "scissors";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const moves = ["rock", "paper", "scissors"];
+  const playerMoveIndex = moves.indexOf(playerSelection);
+  const computerMoveIndex = moves.indexOf(computerSelection);
+  if (playerMoveIndex - computerMoveIndex === 1 ||
+      playerMoveIndex - computerMoveIndex === -2) {
+    return "win";
+  }
+  else if (playerMoveIndex - computerMoveIndex === -1 ||
+      playerMoveIndex - computerMoveIndex === 2){
+    return "lose";
+  }
+}
+
+function gameOutcome(numberOfWins, numberOfLosses) {
+  if (numberOfWins > numberOfLosses) {
+    return "You Win!";
+  }
+  else if (numberOfWins < numberOfLosses) {
+    return "You Lose!";
+  }
+  else {
+    return "It's a Tie!";
+  }
+}
+
+function game() {
+  let numberOfWins = 0;
+  let numberOfLosses = 0;
+
+  for (let round = 0; round < 5; round++) {
+    const playerSelection = prompt("Enter a move (ex. rock): ").toLowerCase();
+    const computerSelection = computerPlay();
+    let result = playRound(playerSelection, computerSelection);
+    console.log("Round " + (round + 1) + " computer chose " + 
+        computerSelection + " - you " + result);
+
+    if (result === "win") {
+      numberOfWins += 1;
+    }
+    else if (result === "lose") {
+      numberOfLosses += 1;
+    }
+  }
+
+  console.log(gameOutcome(numberOfWins, numberOfLosses));
+}
+
+game();
